20491a7bbafa78ac398a0cbce65b2662002603b0
jrwatson94 <j.watsonreid@gmail.com>
2020-11-16 18:14:27 -0500

    Done

--- src/Containers/NotesContainer.js
+++ src/Containers/NotesContainer.js
@@ -2,23 +2,22 @@ import React from 'react';
 import Note from '../Components/Note.js';
 
 class NotesContainer extends React.Component {
-
     apiResponse = () => {
         return [{ id: 1, content: "First Note" }, { id: 2, content: "Second Note" }, { id: 3, content: "Third Note" }, { id: 4, content: "Fourth Note" }]
     }
 
     renderNotes = () => {
-        return this.apiResponse().map( note => <Note key={Note.id} note={note} />)
+        return 
     }
 
     render() {
         return (
             <ul>
-                {this.renderNotes()}
+                {this.apiResponse().map( note => <Note key={Note.id} note={note} />)}
             </ul>
+                
         )
     }
-
 }
 
 export default NotesContainer;
