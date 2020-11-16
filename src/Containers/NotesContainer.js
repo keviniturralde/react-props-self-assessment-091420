3e1e758c829cfeee351871c87d3ac8ff3dd60f6a
github username <github email address>
2020-11-16 10:18:24 -0500

    having issues understanding the material

--- src/Containers/NotesContainer.js
+++ src/Containers/NotesContainer.js
@@ -1,14 +1,19 @@
 import React from 'react';
+// import Note from './Components/Note'
+
 class NotesContainer extends React.Component {
 
     apiResponse = () => {
+        // console.log(this.props)
         return [{ id: 1, content: "First Note" }, { id: 2, content: "Second Note" }, { id: 3, content: "Third Note" }, { id: 4, content: "Fourth Note" }]
     }
 
     render() {
+        
         return (
+            
             <ul>
-                {}
+               
             </ul>
         )
     }
