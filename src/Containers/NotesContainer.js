70f912c347526392c542fc425d1aa4dbf91b6a1a
Matthew Kirby <matt.kirby@yahoo.com>
2020-11-16 10:23:04 -0500

    passing tests but list items not renderering

--- src/Containers/NotesContainer.js
+++ src/Containers/NotesContainer.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import Note from '../Components/Note';
+
 class NotesContainer extends React.Component {
 
     apiResponse = () => {
         return [{ id: 1, content: "First Note" }, { id: 2, content: "Second Note" }, { id: 3, content: "Third Note" }, { id: 4, content: "Fourth Note" }]
     }
 
+    renderEachNote = () => {
+        return this.apiResponse().map((note) => <Note note={{key: note.id, content: note.content}} />)
+    }
+
     render() {
         return (
             <ul>
-                {}
+                {this.renderEachNote()}
             </ul>
         )
     }
