3b71462f654a85afdbf7cda1ff31f301692bb3f9
gregaswright <greg.a.s.wright@gmail.com>
2020-11-16 10:38:52 -0500

    gregWright

--- src/Containers/NotesContainer.js
+++ src/Containers/NotesContainer.js
@@ -4,7 +4,8 @@ class NotesContainer extends React.Component {
     apiResponse = () => {
         return [{ id: 1, content: "First Note" }, { id: 2, content: "Second Note" }, { id: 3, content: "Third Note" }, { id: 4, content: "Fourth Note" }]
     }
-
+ 
+    // still need a little fuzzy on some of the things, did not have the time over the weekend to cover as much as I wanted but I'll catch up.
     render() {
         return (
             <ul>
