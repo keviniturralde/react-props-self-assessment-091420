bc5387298f3ed05f522bc88fe7699d4d4526cf5e
jayelle0 <you@example.com>
2020-11-16 10:27:14 -0500

    done

--- src/Containers/NotesContainer.js
+++ src/Containers/NotesContainer.js
@@ -1,14 +1,20 @@
 import React from 'react';
-class NotesContainer extends React.Component {
 
-    apiResponse = () => {
-        return [{ id: 1, content: "First Note" }, { id: 2, content: "Second Note" }, { id: 3, content: "Third Note" }, { id: 4, content: "Fourth Note" }]
-    }
+ const apiResponse = () => {
+    return [{ id: 1, content: "First Note" }, { id: 2, content: "Second Note" }, { id: 3, content: "Third Note" }, { id: 4, content: "Fourth Note" }]
+}
+
+// console.log (apiResponse())
+
+let notes = apiResponse().map(x => <li>{x.content}</li> )
+
+console.log(notes)
+class NotesContainer extends React.Component {
 
     render() {
         return (
             <ul>
-                {}
+               {notes}
             </ul>
         )
     }
