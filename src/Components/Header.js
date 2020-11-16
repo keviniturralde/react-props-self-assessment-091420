3221c52017adc8ca84e2772b6ddb873843bbe7a8
artwilton <Arthur.M.Wilton@gmail.com>
2020-11-16 10:39:59 -0500

    done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-class Header extends React.Component {
-  render() {
-    return <h1>{/*text goes here*/}</h1>;
-  }
+function Header(props) {
+  console.log(props)
+    return <h1>{props.text}</h1>
+  
 }
 
 export default Header;
--- src/Containers/NotesContainer.js
+++ src/Containers/NotesContainer.js
@@ -1,19 +1,38 @@
 import React from 'react';
-class NotesContainer extends React.Component {
 
-    apiResponse = () => {
+function NotesContainer() {
+    const apiResponse = () => {
         return [{ id: 1, content: "First Note" }, { id: 2, content: "Second Note" }, { id: 3, content: "Third Note" }, { id: 4, content: "Fourth Note" }]
     }
 
-    render() {
-        return (
+    console.log(apiResponse())
+    // const notes = apiResponse().map(function (note) {return <Note test/>})
+
+
+
+    return (
             <ul>
+                test
                 {}
             </ul>
         )
-    }
+}    
 
-}
+// class NotesContainer extends React.Component {
+
+//     apiResponse = () => {
+//         return [{ id: 1, content: "First Note" }, { id: 2, content: "Second Note" }, { id: 3, content: "Third Note" }, { id: 4, content: "Fourth Note" }]
+//     }
+
+//     render() {
+//         return (
+//             <ul>
+//                 {}
+//             </ul>
+//         )
+//     }
+
+// }
 
 export default NotesContainer;
 
