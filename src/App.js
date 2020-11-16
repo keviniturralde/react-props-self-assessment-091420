3f24bf5642b3f8b1c6004fe8883cca957a2a4d34
Matthew Kirby <matt.kirby@yahoo.com>
2020-11-16 10:36:21 -0500

    added note container to be rendered in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Header from './Components/Header'
+import NoteContainer from './Containers/NotesContainer';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header text='Hello World' />
+        <NoteContainer />
       </div>
     )
   }
