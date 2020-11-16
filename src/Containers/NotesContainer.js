08490b646513f54eeab03525a6d9ffafe802ad2f
WaffleWaldo <oswaldoortiz0519@gmail.com>
2020-11-16 10:03:06 -0500

    Done

--- src/Containers/NotesContainer.js
+++ src/Containers/NotesContainer.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import "../Components/Note"
+
 class NotesContainer extends React.Component {
 
     apiResponse = () => {
         return [{ id: 1, content: "First Note" }, { id: 2, content: "Second Note" }, { id: 3, content: "Third Note" }, { id: 4, content: "Fourth Note" }]
     }
 
+    renderNote = () => {        //Note should be from the Note component, however it says its not defined.
+        this.apiResponse().map((note) => <Note key={note.id} note={note} />)
+    }
+
     render() {
         return (
             <ul>
-                {}
+                {this.renderNote()}
             </ul>
         )
     }
