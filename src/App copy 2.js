769e2c6707eb65ad9c9dc380f30b244154dfb7f9
JetechNY <sunjetliu@gmail.com>
2020-11-23 02:27:24 -0500

    done

--- src/App copy 2.js
+++ src/App copy 2.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
-import Header from './Components/Header'
-import NotesContainer from './Containers/NotesContainer'
+import Header from './Components/Header';
+import NotesContainer from './Containers/NotesContainer';
 
 class App extends Component {
   render() {
     return (
-      <>
-        <Header text="Hello World"/>
+      <div>
+        <Header message = "Hello World"/>
         <NotesContainer/>
-      </>
+      </div>
     )
   }
 }
